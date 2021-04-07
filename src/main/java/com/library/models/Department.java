cf2d57375109ff4ff7b1a11586dcb5783f7daf04
nwickh03 <79473056+nwickh03@users.noreply.github.com>
2021-04-06 23:43:30 -0400

    Department.java Bean implemented without Spring.

--- src/main/java/com/library/models/Department.java
+++ src/main/java/com/library/models/Department.java
@@ -1,4 +1,55 @@
 package com.library.models;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Set;
+
+@Entity
+@Table(name = "departments")
 public class Department {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    @ManyToMany
+    //owner/parent side of mapping defines join table
+    @JoinTable(name = "Department_Book",
+        joinColumns = {@JoinColumn(name = "department_id")},
+        inverseJoinColumns = {@JoinColumn(name = "book_id")})
+    private Set<Book> books;
+
+    public Department() {
+    }
+
+    public Department(int id, String name, Set<Book> books) {
+        this.id = id;
+        this.name = name;
+        this.books = books;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
 }
