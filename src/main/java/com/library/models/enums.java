8e730867a851c2b74526a474e4de98bc40983aea
nwickh03 <79473056+nwickh03@users.noreply.github.com>
2021-04-09 18:39:51 -0400

    security

--- src/main/java/com/library/models/enums.java
+++ src/main/java/com/library/models/enums.java
@@ -1,8 +1,21 @@
 package com.library.models;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public class enums {
     public enum AccountType{
-        PATRON, LIBRARIAN, ADMIN
+        PATRON, LIBRARIAN, ADMIN;
+
+        public SimpleGrantedAuthority toAuth(){
+            if (this == ADMIN)
+                return new SimpleGrantedAuthority("ADMIN");
+            else if(this == PATRON)
+                return new SimpleGrantedAuthority("PATRON");
+            else if (this == LIBRARIAN)
+                return new SimpleGrantedAuthority("LIBRARIAN");
+            else return null;
+        }
+
     }
 
     public enum FeeType{
